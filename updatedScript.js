521c4e1653744ddafc88e31dde35b9230156073d
isjaki <usow.aleksey@yandex.ru>
2019-03-25 22:03:20 +0400

    began to create functions, added _createCalendarTemplate, _createCalendarCaption and _createCalenderHeader functions to prototype and created variables in them, added "_" character to the beginning of the properties' names, to show that they are only for internal use and are not accessible from outside

--- updatedScript.js
+++ updatedScript.js
@@ -1,14 +1,24 @@
 var DAYS_OF_WEEK = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
 function Calendar(options) {
-    this.year = options.year || new Date().getFullYear();
-    this.month = options.month - 1 || new Date().getMonth();
+    this._year = options.year || new Date().getFullYear();
+    this._month = options.month - 1 || new Date().getMonth();
+    this._enteredDate = new Date(this._year, this._month);
 }
 
 Calendar.prototype._createCalendarTemplate = function() {
+    const calendarTable = document.createElement('table');
+    const tableBody = document.createElement('tbody');
+
 
 }
 
 Calendar.prototype._createCalendarCaption = function() {
+    const tableCaption = document.createElement('caption');
 
+
+}
+
+Calendar.prototype._createCalendarHeader = function() {
+    const tableHeader = document.createElement('tr');
 }

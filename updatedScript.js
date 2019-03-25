6dbd67b5312bc486588868f73f79bad0c756afef
isjaki <usow.aleksey@yandex.ru>
2019-03-25 21:41:58 +0400

    created constructor function, added year and month properties and added default values to them

--- updatedScript.js
+++ updatedScript.js
@@ -1,1 +1,14 @@
-const DAYS_OF_WEEK = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+var DAYS_OF_WEEK = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+function Calendar(options) {
+    this.year = options.year || new Date().getFullYear();
+    this.month = options.month - 1 || new Date().getMonth();
+}
+
+Calendar.prototype._createCalendarTemplate = function() {
+
+}
+
+Calendar.prototype._createCalendarCaption = function() {
+
+}
